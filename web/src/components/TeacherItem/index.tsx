3e85e1563ccfcbec85c1ef98eeef7ec6707a7089
Iury <iury.cavalcante@gmail.com>
2020-08-12 18:00:30 -0300

    initial commit - React frontend

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
+
+import './styles.css';
+import { prependOnceListener } from 'process';
+
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function handleNewConnection() {
+    api.post('connections', { user_id: teacher.id });
+  }
+
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+      <p>{teacher.bio}</p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a
+          target="_blank"
+          href={`https://wa.me/${teacher.whatsapp}`}
+          onClick={handleNewConnection}
+        >
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
